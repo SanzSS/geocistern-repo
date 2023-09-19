2caef394a1c33c2b43a22a0ff750bdfea425c9a7
Sanzhar Shamshiyev <sanpos.kz@gmail.com>
2023-09-18 23:09:03 -0400

    For the most part, implemented/fixed the date filtering, need to determine how to handle ranges and undated objects

--- frontend/src/Pages/ExploreComponents/Explore.jsx
+++ frontend/src/Pages/ExploreComponents/Explore.jsx
@@ -68,9 +68,13 @@ export default function Explore() {
                 // console.log(row);
             }
             // TODO
-            let date = database.indexOf(row["titleOriginal"]).normalizedDate;
-            // console.log(date);
+            // if (database.indexOf(row["titleOriginal"]) == -1){
+            //     return false;
+            // }
+            // let date = database.indexOf(row["titleOriginal"]).normalizedDate;
+            let date = normalizedDate(row["date"]);
             if (date < dateFilter[0] || date > dateFilter[1]) {
+                
                 return false;
             }
             // TODO (dimension filter)
@@ -110,3 +114,17 @@ export default function Explore() {
         </div>
     );
 }
+
+function normalizedDate(date) {
+    if (date[0] === "~"){
+        return Number(date.slice(1, 5));
+    }
+    if (date[0] === "["){
+        return 0;
+    }
+    if (date[3] === " "){
+        return Number(date.slice(0, 4));
+    }
+    return Number(date.slice(0, 5));
+}
+
